ccc0f608f026478e9a0f8c58aa9d5639df959cb1
Seasa_Huang <ericet1234@gmail.com>
2018-11-08 20:09:11 +0800

    count template

--- parser/count.py
+++ parser/count.py
@@ -7,25 +7,43 @@ this file is for counting the number of word in the dataset
 import re
 import string
 from parser import parser
+import sys
+import matplotlib.pyplot as plt
 
 def remove(line):
 	if(line is None):
-		return 
+		return ''
 	line = re.sub('['+string.punctuation+']', ' ', line)
 	line = re.sub('  ', ' ', line)
-	return line
+	return line.lower().strip().split()
 
 def add(vocab,line):
 	if(line is None):
-		return 
-	for word in line.strip().split():
+		return None
+	for word in line:
 		try:
 			vocab[word] += 1
 		except KeyError:
 			vocab[word] = 1
 
-task = 'taskB'
+def compare(stat,num,dtype):
+	try:
+		if(num and num > stat[dtype]['max']):
+			stat[dtype]['max'] = num
+		if(num and num < stat[dtype]['min']):
+			stat[dtype]['min'] = num
+	except KeyError:
+		stat[dtype]['min'] = num
+		stat[dtype]['max'] = num
 
+def count(stat,num,dtype):
+	try:
+		stat[dtype][num] += 1
+	except KeyError:
+		stat[dtype][num] = 1
+
+task = sys.argv[1]
+ID = set()
 
 vocab = {}
 datalist = [
@@ -39,24 +57,57 @@ datalist = [
 		]
 dataloader = parser(datalist,task)
 
+stat = {"Subject":{},"Body":{},"Text":{}}
 
 for data in dataloader.iterator():
-	
-	add(vocab,remove(data["Subject"]))
-	add(vocab,remove(data["Body"]))
+	if(data["ID"] in ID):
+		continue
+	ID.add(data["ID"])
+
+	num = len(remove(data["Subject"]))
+	count(stat,num,"Subject")
+	num = len(remove(data["Body"]))
+	count(stat,num,"Body")
+
 	if("Comment" in data):
 		for comment in data["Comment"]:
-			add(vocab,remove(comment["Text"]))
+			if(comment["ID"] in ID):
+				continue
+			ID.add(comment["ID"])
 
-	if("RelQuestion" in data):
-		for comment in data["RelQuestion"]:
-			add(vocab,remove(comment["Subject"]))
-			add(vocab,remove(comment["Body"]))
+			num = len(remove(comment["Text"]))
+			count(stat,num,"Text")
 
-arr = [ (name,vocab[name]) for name in vocab]
-arr = sorted(arr,key=lambda x:x[1],reverse=True)
+	elif("RelQuestion" in data):
+		for ques in data["RelQuestion"]:
+			if(ques["ID"] in ID):
+				continue
+			ID.add(ques["ID"])
 
-with open('vocab.'+task,'w') as f:
-	for data in arr:
-		f.write('{0} {1}\n'.format(data[0],data[1]))
+			num = len(remove(ques["Subject"]))
+			count(stat,num,"Subject")
+			num = len(remove(ques["Body"]))
+			count(stat,num,"Body")
+
+	elif("Thread" in data):
+		for thread in data["Thread"]:
+			num = len(remove(thread["RelQuestion"]["Subject"]))
+			count(stat,num,"Subject")
+			num = len(remove(thread["RelQuestion"]["Body"]))
+			count(stat,num,"Body")
+
+			for comment in thread["Comment"]:
+				num = len(remove(comment["Text"]))
+				count(stat,num,"Text")
+
+
+for dtype in stat:
+	arr = []
+	for num in stat[dtype]:
+		while(len(arr) <= num):
+			arr.append(0)
+		arr[num] = stat[dtype][num]
+	
+	plt.plot(list(range(len(arr))),arr)
+	plt.show()
 
