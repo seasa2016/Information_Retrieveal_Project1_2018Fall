26aa0bafa4896a1dfd1d6ba6b7e302a2b242ea3c
Seasa_Huabg <ericet1234@gmail.com>
2018-11-23 12:54:00 +0800

    inference part

--- seasa/test.py
+++ seasa/test.py
@@ -20,8 +20,8 @@ def get_data(batch_size):
 		'./data/semeval/training_data/SemEval2016-Task3-CQA-QL-test.xml'
 	]
 
-	test_dataset = itemDataset( file_name=test_file,vocab='./data/vocab',
-								transform=transforms.Compose([ToTensor()]))
+	test_dataset = itemDataset( file_name=test_file,
+							vocab='./vocab',task='taskA',transform=transforms.Compose([ToTensor()]))
 	test_dataloader = DataLoader(test_dataset, batch_size=batch_size,shuffle=False, num_workers=16,collate_fn=collate_fn)
     
 	length = len(test_dataloader)
@@ -30,10 +30,12 @@ def get_data(batch_size):
 
 def convert(data,device):
 	for name in data:
+		if(name[-2:]=='ID'):
+			continue
 		data[name] = data[name].to(device)
 	return data
 
-def train(args):
+def test(args):
 	print("check device")
 	if(torch.cuda.is_available() and args.gpu>=0):
 		device = torch.device('cuda')
@@ -54,139 +56,37 @@ def train(args):
 	elif(args.model == 'bimpm'):
 		model = bimpm(args)
 
+	model.load_state_dict(torch.load(args.load)
+
 	model = model.to(device=device)
 
 	print(model)
-	optimizer = optim.Adam(model.parameters(),lr=args.learning_rate)
-	criterion = nn.BCEWithLogitsLoss(reduction='sum')
 	
-	loss_best = 100000000
 	print("start training")
-	for now in range(args.epoch):
-		print(now)
 		
-		Loss = {'class':0,'rank':0}
-		Count = {'class':0,'rank':0}
-		temp_Loss = {'class':0,'rank':0}
-		temp_Count = {'class':0,'rank':0}
-
-		model.train()
-		for i,data in enumerate(dataloader['train']):
-			#print(i)
-			model.zero_grad()
-
-			#first convert the data into cuda
-			data = convert(data,device)
-
-			#deal with the classfication part
-			out_left = model.encoder(data['query'],data['query_len'],data['left'],data['left_len'])
-			out_left = model.decoder(out_left)
-			pred = (out_left.sigmoid()>0.5).int()
-			
-			temp_Count['class'] += ( data['left_type'].int()==pred ).sum()
-			Count['class'] += ( data['left_type'].int()==pred ).sum()
-			
-			loss = criterion(out_left,data['left_type']) 
-			loss.backward(retain_graph=True)
-			
-			temp_Loss['class'] += loss.detach().cpu().item()
-			Loss['class'] += loss.detach().cpu().item()
-
-			out_right = model.encoder(data['query'],data['query_len'],data['right'],data['right_len'])
-			out_right = model.decoder(out_right)
-			pred = (out_right.sigmoid()>0.5).int()
-			temp_Count['class'] += ( data['right_type'].int()==pred ).sum()
-			Count['class'] += ( data['right_type'].int()==pred ).sum()
-			
-			loss = criterion(out_right,data['right_type']) 
-			loss.backward(retain_graph=True)
-			
-			temp_Loss['class'] += loss.detach().cpu().item()
-			Loss['class'] += loss.detach().cpu().item()
-
-			#deal with the ranking part
-			out = out_left-out_right
-			pred = (out.gt(0).int())==data['total_type'].int()
-
-			temp_Count['rank'] +=  pred.sum()
-			Count['rank'] +=  pred.sum()
-			
-			#loss = -(out*data['total_type']).sum()
-			loss = criterion(out,data['total_type']) 
-			
-			temp_Loss['rank'] = loss.detach().cpu().item()
-			Loss['rank'] += loss.detach().cpu().item()
-			
-			loss.backward()
-			
-			optimizer.step()
-			if(i%20==0):
-				#print('out',out_right.sigmoid().view(-1))
-				#print('label',data['right_type'].view(-1))
-				print(i,' training loss(class):{0} loss(rank):{1} acc:{2}/{3} {4}/{5}'.format(temp_Loss['class'],temp_Loss['rank'],temp_Count['class'],args.batch_size*40,temp_Count['rank'],args.batch_size*20))
-
-				temp_Loss = {'class':0,'rank':0}
-				temp_Count = {'class':0,'rank':0}
-		
-		if(now%args.print_freq==0):
-			print('*'*10)
-			print('training loss(class):{0} loss(rank):{1} acc:{2}/{3} {4}/{5}'.format(
-							Loss['class']/length['train']/2,Loss['rank']/length['train'],Count['class'],length['train']*2,Count['rank'],length['train']))
-		
-
-
-		Loss = {'class':0,'rank':0}
-		Count = {'class':0,'rank':0}
-
+	with open(args.output,'w') as f:
 		model.eval()
-		for i,data in enumerate(dataloader['valid']):
+		for i,data in enumerate(dataloader):
 			#print(i)
 			with torch.no_grad():
 				#first convert the data into cuda
 				data = convert(data,device)
 
 				#deal with the classfication part
-				out_left = model.encoder(data['query'],data['query_len'],data['left'],data['left_len'])
-				out_left = model.decoder(out_left)
-				pred = (out_left.sigmoid()>0.5).int()
-				Count['class'] += ( data['left_type'].int()==pred ).sum()
-				loss = criterion(out_left,data['left_type']) 
-				Loss['class'] += loss.detach().cpu().item()
-
-				out_right = model.encoder(data['query'],data['query_len'],data['right'],data['right_len'])
-				out_right = model.decoder(out_right)
-				pred = (out_right.sigmoid()>0.5).int()
-				Count['class'] += ( data['right_type'].int()==pred ).sum()
-				loss = criterion(out_right,data['right_type']) 
-				Loss['class'] += loss.detach().cpu().item()
-
-
-				#deal with the ranking part
-				out = out_left-out_right
+				out_left = model.encoder(data['query'],data['query_len'],data['answer'],data['answer_len'])
+				out_left = model.decoder(out_left).detach().cpu()
+				ans = (out_left.sigmoid()>0.5).detach().cpu()
 				
-				loss = criterion(out,data['total_type']) 
-				Count['rank'] +=  (out.gt(1e-5).int()==data['total_type'].int()).sum()
-
-				Loss['rank'] += loss.detach().cpu().item()
-
-		if(now%args.print_freq==0):
-			print('*'*10)
-			print(i,' testing loss(class):{0} loss(rank):{1} acc:{2}/{3} {4}/{5}'.format(
-							Loss['class']/length['valid']/2,Loss['rank']/length['valid'],Count['class'],length['valid']*2,Count['rank'],length['valid']))
-		
-		
-		torch.save(model.state_dict(), './saved_models/{0}/step_{1}.pkl'.format(args.save,now))
-
-		if(Loss['class']<loss_best):
-			torch.save(model.state_dict(), './saved_models/{0}/best.pkl'.format(args.save))
-			loss_best = Loss['class']
+				for i in range(out_left.shape[0]):
+					f.write('{0}\t{1}\t{2}\t{3}\t{4}'.format(
+						data['answer_ID'][i],data['query_ID'][i],i,out_left[i],ans[i]
+					))
 
 def main():
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('--batch_size', default=1024, type=int)
 	parser.add_argument('--dropout', default=0, type=float)
-	parser.add_argument('--epoch', default=200, type=int)
 	parser.add_argument('--gpu', default=0, type=int)
 	
 	parser.add_argument('--word_dim', default=64, type=int)
@@ -196,25 +96,18 @@ def main():
 	parser.add_argument('--learning_rate', default=0.005, type=float)
 	parser.add_argument('--model', default="qa_lstm", type=str)
 
-	parser.add_argument('--print_freq', default=1, type=int)
-
-	parser.add_argument('--save', required=True , type=str)
+	parser.add_argument('--output', required=True , type=str)
+	parser.add_argument('--load', required=True , type=str)
 	
 	args = parser.parse_args()
 
 	setattr(args, 'input_size', 49526+1)
 	setattr(args,'batch_first',True)
 	setattr(args, 'class_size',1)
-
-	if not os.path.exists('saved_models'):
-		os.makedirs('saved_models')
-
-	if not os.path.exists('./saved_models/{0}'.format(args.save)):
-		os.makedirs('./saved_models/{0}'.format(args.save))
-
-	print('training start!')
-	train(args)
-	print('training finished!')
+	
+	print('testing start!')
+	test(args)
+	print('testing finished!')
 	
 
 
